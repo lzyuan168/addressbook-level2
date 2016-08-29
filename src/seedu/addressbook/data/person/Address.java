225b7bc322a2676330d9a0fec959a87bd301572e
LENOVO-PC\Zhiyuan <lzyuan168@gmail.com>
2016-08-29 23:51:27 +0800

    T3A3 ImplementClass

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,9 +8,14 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String EXAMPLE = "a/123, Clementi Ave 3, #12-34, 231534";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses to be in this format";
+    public static final String ADDRESS_VALIDATION_REGEX = ",+";
+    
+    private final Block block;
+    private final Street street;
+    private final Unit unit;
+    private final PostalCode postalCode;
 
     public final String value;
     private boolean isPrivate;
@@ -26,10 +31,15 @@ public class Address {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
         this.value = address;
+        String[] fullAddress = address.split(", ");
+        this.block = new Block(fullAddress[0].trim());
+        this.street = new Street(fullAddress[1].trim());
+        this.unit = new Unit(fullAddress[2].trim());
+        this.postalCode = new PostalCode(fullAddress[3].trim());
     }
 
     /**
-     * Returns true if a given string is a valid person email.
+     * Returns true if a given string is a valid person address.
      */
     public static boolean isValidAddress(String test) {
         return test.matches(ADDRESS_VALIDATION_REGEX);
